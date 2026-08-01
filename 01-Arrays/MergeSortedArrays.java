3b33dbb519209f3bbc5d7b3c07deeabcba41d8c7
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-08-01 08:02:59 +0100

    Add merge-and-sort two arrays program
    Merges two arrays into one sorted array.

---
+++ 01-Arrays/MergeSortedArrays.java
@@ -0,0 +1,27 @@
+public class MergeSortedArrays {
+
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int[] merged = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] <= arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+        while (i < arr1.length) merged[k++] = arr1[i++];
+        while (j < arr2.length) merged[k++] = arr2[j++];
+
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {5, 2, 9};
+        int[] arr2 = {1, 4, 7};
+
+        int[] merged = mergeArrays(arr1, arr2);
+        for (int val : merged) System.out.print(val + " ");
+    }
+}
